18ccbc23239017c3abb70517e801e36bfd28b25e
Alan Garny <agarny@hellix.com>
2020-04-13 12:06:46 +1200

    Minor improvement to our setup.py script.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     author='Alan Garny',
@@ -6,7 +6,7 @@ setup(
     description='OpenCOR-based Python script to model Covid-19 using the SEIR model',
     license='Apache 2.0',
     name='seir',
-    packages=['seir'],
+    packages=find_packages('src'),
     package_dir={'': 'src'},
     url='https://github.com/ABI-Covid-19/seir',
     version='0.2.0',
