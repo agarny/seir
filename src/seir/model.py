5bf5fe981790c9b59388924f875231caea589752
Alan Garny <agarny@hellix.com>
2020-04-15 11:44:58 +1200

    Use a proper class structure to keep track of our simulation results (#15).

--- src/seir/model.py
+++ src/seir/model.py
@@ -1,4 +1,5 @@
 import os
+from enum import Enum, auto
 
 import numpy as np
 import opencor as oc
@@ -9,6 +10,43 @@ class Model:
     SEIR model of Covid-19, as described at
     https://cpb-ap-se2.wpmucdn.com/blogs.auckland.ac.nz/dist/d/75/files/2017/01/Covid19_SEIR_model.pdf.
     """
+
+    class Parameter:
+        """
+        A model parameter, i.e. either a state variable or an algebraic variable.
+        """
+
+        class Kind(Enum):
+            VOI = auto()
+            STATE = auto()
+            ALGEBRAIC = auto()
+
+        def __init__(self, kind, parameter):
+            # Initialise our model parameter.
+
+            self._kind = kind
+            self._parameter = parameter
+            self._values = np.array([])
+
+        def kind(self):
+            # Return our kind.
+
+            return self._kind
+
+        def name(self):
+            # Return our name.
+
+            return self._parameter.name()
+
+        def values(self):
+            # Return our values.
+
+            return self._values
+
+        def _append_values(self, values):
+            # Append the given values to our internal values.
+
+            self._values = np.append(self._values, values)
 
     def __init__(self):
         # Initialise our SEIR simulation.
@@ -22,41 +60,20 @@ class Model:
         states = results.states()
         algebraic = results.algebraic()
 
-        self.voi = results.voi()
-        self.voi_values = np.array([])
+        self.voi = self.Parameter(self.Parameter.Kind.VOI, results.voi())
 
-        self.s = states['main/S']
-        self.s_values = np.array([])
+        self.s = self.Parameter(self.Parameter.Kind.STATE, states['main/S'])
+        self.e = self.Parameter(self.Parameter.Kind.STATE, states['main/E'])
+        self.i_c = self.Parameter(self.Parameter.Kind.STATE, states['main/I_c'])
+        self.i_p = self.Parameter(self.Parameter.Kind.STATE, states['main/I_p'])
+        self.i_u = self.Parameter(self.Parameter.Kind.STATE, states['main/I_u'])
+        self.r_c = self.Parameter(self.Parameter.Kind.STATE, states['main/R_c'])
+        self.r_u = self.Parameter(self.Parameter.Kind.STATE, states['main/R_u'])
 
-        self.e = states['main/E']
-        self.e_values = np.array([])
-
-        self.i_c = states['main/I_c']
-        self.i_c_values = np.array([])
-
-        self.i_p = states['main/I_p']
-        self.i_p_values = np.array([])
-
-        self.i_u = states['main/I_u']
-        self.i_u_values = np.array([])
-
-        self.r_c = states['main/R_c']
-        self.r_c_values = np.array([])
-
-        self.r_u = states['main/R_u']
-        self.r_u_values = np.array([])
-
-        self.i = algebraic['main/I']
-        self.i_values = np.array([])
-
-        self.r = algebraic['main/R']
-        self.r_values = np.array([])
-
-        self.d = algebraic['main/D']
-        self.d_values = np.array([])
-
-        self.ifr = algebraic['main/IFR']
-        self.ifr_values = np.array([])
+        self.i = self.Parameter(self.Parameter.Kind.ALGEBRAIC, algebraic['main/I'])
+        self.r = self.Parameter(self.Parameter.Kind.ALGEBRAIC, algebraic['main/R'])
+        self.d = self.Parameter(self.Parameter.Kind.ALGEBRAIC, algebraic['main/D'])
+        self.ifr = self.Parameter(self.Parameter.Kind.ALGEBRAIC, algebraic['main/IFR'])
 
     def run(self, sim_duration=300):
         # Make sure that we were given a valid simulation duration.
@@ -84,18 +101,18 @@ class Model:
             # Update our simulation results using the results of the current
             # simulation.
 
-            self.voi_values = np.append(self.voi_values, run_nb + self.voi.values())
-            self.s_values = np.append(self.s_values, self.s.values())
-            self.e_values = np.append(self.e_values, self.e.values())
-            self.i_c_values = np.append(self.i_c_values, self.i_c.values())
-            self.i_p_values = np.append(self.i_p_values, self.i_p.values())
-            self.i_u_values = np.append(self.i_u_values, self.i_u.values())
-            self.r_c_values = np.append(self.r_c_values, self.r_c.values())
-            self.r_u_values = np.append(self.r_u_values, self.r_u.values())
-            self.i_values = np.append(self.i_values, self.i.values())
-            self.r_values = np.append(self.r_values, self.r.values())
-            self.d_values = np.append(self.d_values, self.d.values())
-            self.ifr_values = np.append(self.ifr_values, self.ifr.values())
+            self.voi._append_values(run_nb + self.voi._parameter.values())
+            self.s._append_values(self.s._parameter.values())
+            self.e._append_values(self.e._parameter.values())
+            self.i_c._append_values(self.i_c._parameter.values())
+            self.i_p._append_values(self.i_p._parameter.values())
+            self.i_u._append_values(self.i_u._parameter.values())
+            self.r_c._append_values(self.r_c._parameter.values())
+            self.r_u._append_values(self.r_u._parameter.values())
+            self.i._append_values(self.i._parameter.values())
+            self.r._append_values(self.r._parameter.values())
+            self.d._append_values(self.d._parameter.values())
+            self.ifr._append_values(self.ifr._parameter.values())
 
             run_nb += 1
 
@@ -107,28 +124,28 @@ class Model:
         plt.gcf().canvas.set_window_title('SEIR model')
 
         plt.subplot(4, 1, 1)
-        plt.plot(self.voi_values, self.s_values, label=self.s.name())
-        plt.plot(self.voi_values, self.e_values, label=self.e.name())
-        plt.plot(self.voi_values, self.i_p_values, label=self.i_p.name())
-        plt.plot(self.voi_values, self.i_values, label=self.i.name())
-        plt.plot(self.voi_values, self.r_values, label=self.r.name())
-        plt.plot(self.voi_values, self.d_values, label=self.d.name())
+        plt.plot(self.voi.values(), self.s.values(), label=self.s.name())
+        plt.plot(self.voi.values(), self.e.values(), label=self.e.name())
+        plt.plot(self.voi.values(), self.i_p.values(), label=self.i_p.name())
+        plt.plot(self.voi.values(), self.i.values(), label=self.i.name())
+        plt.plot(self.voi.values(), self.r.values(), label=self.r.name())
+        plt.plot(self.voi.values(), self.d.values(), label=self.d.name())
         plt.legend(loc='center left')
 
         plt.subplot(4, 1, 2)
-        plt.plot(self.voi_values, self.i_values, label=self.i.name())
-        plt.plot(self.voi_values, self.i_c_values, label=self.i_c.name())
-        plt.plot(self.voi_values, self.i_u_values, label=self.i_u.name())
+        plt.plot(self.voi.values(), self.i.values(), label=self.i.name())
+        plt.plot(self.voi.values(), self.i_c.values(), label=self.i_c.name())
+        plt.plot(self.voi.values(), self.i_u.values(), label=self.i_u.name())
         plt.legend(loc='center left')
 
         plt.subplot(4, 1, 3)
-        plt.plot(self.voi_values, self.r_values, label=self.r.name())
-        plt.plot(self.voi_values, self.r_c_values, label=self.r_c.name())
-        plt.plot(self.voi_values, self.r_u_values, label=self.r_u.name())
+        plt.plot(self.voi.values(), self.r.values(), label=self.r.name())
+        plt.plot(self.voi.values(), self.r_c.values(), label=self.r_c.name())
+        plt.plot(self.voi.values(), self.r_u.values(), label=self.r_u.name())
         plt.legend(loc='center left')
 
         plt.subplot(4, 1, 4)
-        plt.plot(self.voi_values, self.ifr_values, label=self.ifr.name())
+        plt.plot(self.voi.values(), self.ifr.values(), label=self.ifr.name())
         plt.legend(loc='center left')
         plt.xlabel('time (day)')
 
